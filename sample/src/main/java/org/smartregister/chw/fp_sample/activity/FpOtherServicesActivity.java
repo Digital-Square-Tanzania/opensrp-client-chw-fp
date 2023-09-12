1e90573e55cc0748a0219a9d7e0df35182478575
cozej4 <ilakozejumanne@gmail.com>
2023-09-12 15:24:26 +0300

    :hammer: code refactoring

--- sample/src/main/java/org/smartregister/chw/fp_sample/activity/FpOtherServicesActivity.java
+++ sample/src/main/java/org/smartregister/chw/fp_sample/activity/FpOtherServicesActivity.java
@@ -1,16 +1,22 @@
 package org.smartregister.chw.fp_sample.activity;
 
+import static org.smartregister.chw.fp.util.Constants.ENCOUNTER_TYPE;
+
 import android.app.Activity;
 import android.content.Intent;
 
 import com.vijay.jsonwizard.constants.JsonFormConstants;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.smartregister.chw.fp.activity.BaseFpOtherServicesActivity;
 import org.smartregister.chw.fp.domain.MemberObject;
 import org.smartregister.chw.fp.presenter.BaseFpVisitPresenter;
 import org.smartregister.chw.fp.util.Constants;
+import org.smartregister.chw.fp_sample.R;
 import org.smartregister.chw.fp_sample.interactor.FpScreeningVisitInteractor;
+
+import timber.log.Timber;
 
 public class FpOtherServicesActivity extends BaseFpOtherServicesActivity {
     public static void startMe(Activity activity, String baseEntityID, Boolean isEditMode) {
@@ -39,5 +45,20 @@ public class FpOtherServicesActivity extends BaseFpOtherServicesActivity {
         }
 
         startActivityForResult(intent, Constants.REQUEST_CODE_GET_JSON);
+    }
+
+    @Override
+    public void submittedAndClose() {
+        Intent returnIntent = new Intent();
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject = new JSONObject(actionList.get(getString(R.string.fp_other_services)).getJsonPayload());
+            jsonObject.put(ENCOUNTER_TYPE, Constants.EVENT_TYPE.FP_OTHER_SERVICES);
+        } catch (JSONException e) {
+            Timber.e(e);
+        }
+        returnIntent.putExtra(Constants.JSON_FORM_EXTRA.JSON, jsonObject.toString());
+        setResult(Activity.RESULT_OK, returnIntent);
+        close();
     }
 }
--- sample/src/main/java/org/smartregister/chw/fp_sample/activity/FpScreeningActivity.java
+++ sample/src/main/java/org/smartregister/chw/fp_sample/activity/FpScreeningActivity.java
@@ -1,16 +1,22 @@
 package org.smartregister.chw.fp_sample.activity;
 
+import static org.smartregister.chw.fp.util.Constants.ENCOUNTER_TYPE;
+
 import android.app.Activity;
 import android.content.Intent;
 
 import com.vijay.jsonwizard.constants.JsonFormConstants;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.smartregister.chw.fp.activity.BaseFpScreeningActivity;
 import org.smartregister.chw.fp.domain.MemberObject;
 import org.smartregister.chw.fp.presenter.BaseFpVisitPresenter;
 import org.smartregister.chw.fp.util.Constants;
+import org.smartregister.chw.fp_sample.R;
 import org.smartregister.chw.fp_sample.interactor.FpScreeningVisitInteractor;
+
+import timber.log.Timber;
 
 public class FpScreeningActivity extends BaseFpScreeningActivity {
     public static void startMe(Activity activity, String baseEntityID, Boolean isEditMode) {
@@ -39,5 +45,20 @@ public class FpScreeningActivity extends BaseFpScreeningActivity {
         }
 
         startActivityForResult(intent, Constants.REQUEST_CODE_GET_JSON);
+    }
+
+    @Override
+    public void submittedAndClose() {
+        Intent returnIntent = new Intent();
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject = new JSONObject(actionList.get(getString(R.string.fp_medical_eligibility_criteria)).getJsonPayload());
+            jsonObject.put(ENCOUNTER_TYPE, Constants.EVENT_TYPE.FP_SCREENING);
+        } catch (JSONException e) {
+            Timber.e(e);
+        }
+        returnIntent.putExtra(Constants.JSON_FORM_EXTRA.JSON, jsonObject.toString());
+        setResult(Activity.RESULT_OK, returnIntent);
+        close();
     }
 }
