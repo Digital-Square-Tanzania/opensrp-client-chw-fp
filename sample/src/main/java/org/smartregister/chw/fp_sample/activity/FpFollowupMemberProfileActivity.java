c6e5bc39b489383ce5336e2b35bb221112b9a79b
fredsabuni <fredmalack15@gmail.com>
2023-09-13 23:29:23 +0300

    Pull latest FP updates

--- sample/src/main/java/org/smartregister/chw/fp_sample/activity/FpFollowupMemberProfileActivity.java
+++ sample/src/main/java/org/smartregister/chw/fp_sample/activity/FpFollowupMemberProfileActivity.java
@@ -108,12 +108,15 @@ public class FpFollowupMemberProfileActivity extends BaseFpProfileActivity {
         JSONObject jsonForm = FileSourceFactoryHelper.getFileSource("").getFormFromFile(getApplicationContext(), formName);
 
         if (formName.equalsIgnoreCase(Constants.FORMS.FP_PROVISION_OF_FP_METHOD) && jsonForm != null) {
-            jsonForm.getJSONObject("global").put("fp_method_selected", memberObject.getFpMethod());
-            jsonForm.getJSONObject("global").put("sex", memberObject.getGender());
+//            jsonForm.getJSONObject("global").put("fp_method_selected", memberObject.getFpMethod());
+//            jsonForm.getJSONObject("global").put("sex", memberObject.getGender());
+            jsonForm.getJSONObject("global").put("fp_method_selected", "pop");
+            jsonForm.getJSONObject("global").put("sex", "Female");
         }
 
         if (formName.equalsIgnoreCase(Constants.FORMS.FP_COUNSELING) && jsonForm != null) {
-            jsonForm.getJSONObject("global").put("sex", memberObject.getGender());
+//            jsonForm.getJSONObject("global").put("sex", memberObject.getGender());
+            jsonForm.getJSONObject("global").put("sex", "Male");
         }
 
         String currentLocationId = "Tanzania";
